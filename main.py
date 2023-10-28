fa6f059d42b7449064c12c93e4435e166feb581c
CircleTenThanks <ldoqfprfkb@gmail.com>
2023-10-28 12:26:23 +0900

    Update: カスタマイズ
    * https://github.com/CircleTenThanks/Hinatazaka46Calendar.git をベースに渡邉美穂さんのHPを解析できるように
    * サーバ負荷軽減のため、すでに登録されている予定の検索処理を2段階にしたく、カレンダーへの追加処理から分離した
    * 時刻の表記がバラバラなため、それをある程度吸収するための関数を用意した

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 import time
 import pickle
 import os
+import sys
 from tendo import singleton
 import mojimoji
 import re
@@ -37,90 +38,157 @@ def build_calendar_api():
 
 def remove_blank(text):
     text = text.replace("\n", "")
-    text = re.sub('^ +', '', text)
-    text = re.sub(' +$', '', text)
-    text = mojimoji.zen_to_han(text, kana=False)
+    text = text.replace("\t", "")
+    text = text.strip()
     return text
 
 
-def search_event_each_date(year, month):
-    url = (
-        f"https://www.hinatazaka46.com/s/official/media/list?ima=0000&dy={year}{month}"
-    )
+def get_schedule_list(start_page, end_page):
+
+    schedule_list = []
+    for page in range(start_page, end_page + 1):
+        url = f"https://mihowatanabe.jp/contents/schedule/page/{page}"
+        result = requests.get(url)
+        soup = BeautifulSoup(result.content, features="lxml")
+
+        schedule_list.extend(soup.find_all("li", {"class": "list-item"}))
+
+        time.sleep(1)  # NOTE:サーバーへの負荷を解消
+
+    return schedule_list
+
+
+def get_schedule_info(schedule_list):
+
+    event_time = schedule_list.find("time", {"class": "time"})
+    if event_time != None:
+        event_time = event_time['datetime']
+
+    event_name = schedule_list.find("h3", {"class": "list-title"})
+    if event_name != None:
+        event_name = event_name.contents[0]
+        event_name = remove_blank(event_name)
+
+    event_link = schedule_list.find("a", {"class": "gtm_content_link"})
+    if event_link != None:
+        event_link = event_link['href']
+
+    return event_time, event_name, event_link
+
+
+def get_schedule_time(event_time, url):
+
     result = requests.get(url)
     soup = BeautifulSoup(result.content, features="lxml")
-    page_year = remove_blank(soup.find("div", {"class": "c-schedule__page_year"}).text).replace('年', '')
-    page_month = remove_blank(soup.find("div", {"class": "c-schedule__page_month"}).text).replace('月', '')
 
-    if int(year) != int(page_year) or int(month) != int(page_month):
-        print("Error URL")
-        return
+    schedule_detail = soup.find("div", {"class": "body"})
+    line_list = schedule_detail.find_all("p")
 
-    events_each_date = soup.find_all("div", {"class": "p-schedule__list-group"})
-
-    time.sleep(3)  # NOTE:サーバーへの負荷を解消
-
-    return events_each_date
-
-
-def search_start_and_end_time(event_time_text):
-    has_end = event_time_text[-1] != "~"
-    if has_end:
-        start, end = event_time_text.split("~")
-    else:
-        start = event_time_text.split("~")[0]
-        end = start
-    start += ":00"
-    end += ":00"
-    return start, end
-
-
-def search_event_info(event_each_date):
-    event_date_text = remove_blank(event_each_date.contents[1].text)[
-        :-1
-    ]  # NOTE:曜日以外の情報を取得
-    events_time = event_each_date.find_all("div", {"class": "c-schedule__time--list"})
-    events_name = event_each_date.find_all("p", {"class": "c-schedule__text"})
-    events_category = event_each_date.find_all("div", {"class": "p-schedule__head"},)
-    events_link = event_each_date.find_all("li", {"class": "p-schedule__item"})
-
-    return event_date_text, events_time, events_name, events_category, events_link
-
-
-def search_detail_info(event_name, event_category, event_time, event_link):
-    event_name_text = remove_blank(event_name.text)
-    event_category_text = remove_blank(event_category.contents[1].text)
-    event_time_text = remove_blank(event_time.text)
-    event_link_text = event_link.find("a")["href"]
-    event_link_text = f"https://www.hinatazaka46.com{event_link_text}"
-
-    return event_name_text, event_category_text, event_time_text, event_link_text
-
-def search_active_member(event_link_text):
-    try:
-        result = requests.get(event_link_text)
-        soup = BeautifulSoup(result.content, features="lxml")
-        active_members = soup.find("div", {"class": "c-article__tag"}).findAll("a")
-
-        if active_members is None:
-            return ""
+    for line in line_list:
         
-        active_members_text = "メンバー:"
-        for active_member in active_members:
-            active_members_text = active_members_text + active_member.text + ","
-        active_members_text = active_members_text[:-1]
-        time.sleep(3)  # NOTE:サーバー負荷の解消
-    except AttributeError:
-        return ""
+        line = mojimoji.zen_to_han(line.text, kana=False)
+        line = line.replace("-", "~")
+        line = line.replace("〜", "~")  # 機種依存文字が使われていることがあった
+        line = line.replace("年", "/")
+        line = line.replace("月", "/")
+        line = line.replace("日", "")
+        line = line.replace("時", ":")
+        line = line.replace("分", "")
 
-    return active_members_text
+        # 年月日、開始時分、終了時分まですべて記載されているパターン
+        date_text_arr = re.search(r'(\d{4})/(\d+)/(\d+).+?(\d+):(\d+)~(\d+):(\d+)', line)
+
+        if date_text_arr != None:
+            year = date_text_arr[1]
+            month = date_text_arr[2].zfill(2)
+            day = date_text_arr[3].zfill(2)
+            hour_start = date_text_arr[4].zfill(2)
+            minute_start = date_text_arr[5].zfill(2)
+            hour_end = date_text_arr[6].zfill(2)
+            minute_end = date_text_arr[7].zfill(2)
+
+            if event_time != f'{year}-{month}-{day}':
+                continue
+            else:
+                event_start = over24Hdatetime(year, month, day, f'{hour_start}:{minute_start}')
+                event_end = over24Hdatetime(year, month, day, f'{hour_end}:{minute_end}')
+                return event_start, event_end
+
+        # 年月日、開始時分が記載されているパターン
+        date_text_arr = re.search(r'(\d{4})/(\d+)/(\d+).+?(\d+):(\d+)', line)
+
+        if date_text_arr != None:
+            year = date_text_arr[1]
+            month = date_text_arr[2].zfill(2)
+            day = date_text_arr[3].zfill(2)
+            hour_start = date_text_arr[4].zfill(2)
+            minute_start = date_text_arr[5].zfill(2)
+
+            if event_time != f'{year}-{month}-{day}':
+                continue
+            else:
+                event_start = event_end = over24Hdatetime(year, month, day, f'{hour_start}:{minute_start}')
+                return event_start, event_end
+            
+        # 月日、開始時分、終了時分が記載されているパターン
+        date_text_arr = re.search(r'(\d+)/(\d+).+?(\d+):(\d+)~(\d+):(\d+)', line)
+
+        if date_text_arr != None:
+            year = event_time.split('-')[0]
+            month = date_text_arr[1].zfill(2)
+            day = date_text_arr[2].zfill(2)
+            hour_start = date_text_arr[3].zfill(2)
+            minute_start = date_text_arr[4].zfill(2)
+            hour_end = date_text_arr[5].zfill(2)
+            minute_end = date_text_arr[6].zfill(2)
+
+            if event_time != f'{year}-{month}-{day}':
+                continue
+            else:
+                event_start = over24Hdatetime(year, month, day, f'{hour_start}:{minute_start}')
+                event_end = over24Hdatetime(year, month, day, f'{hour_end}:{minute_end}')
+                return event_start, event_end
+        
+        # 月日、開始時分が記載されているパターン
+        date_text_arr = re.search(r'(\d+)/(\d+).+?(\d+):(\d+)', line)
+
+        if date_text_arr != None:
+            year = event_time.split('-')[0]
+            month = date_text_arr[1].zfill(2)
+            day = date_text_arr[2].zfill(2)
+            hour_start = date_text_arr[3].zfill(2)
+            minute_start = date_text_arr[4].zfill(2)
+
+            if event_time != f'{year}-{month}-{day}':
+                continue
+            else:
+                event_start = event_end = over24Hdatetime(year, month, day, f'{hour_start}:{minute_start}')
+                return event_start, event_end
+            
+        # 月日、開始時が記載されているパターン
+        date_text_arr = re.search(r'(\d+)/(\d+).+?(\d+):', line)
+
+        if date_text_arr != None:
+            year = event_time.split('-')[0]
+            month = date_text_arr[1].zfill(2)
+            day = date_text_arr[2].zfill(2)
+            hour_start = date_text_arr[3].zfill(2)
+            minute_start = "00"
+
+            if event_time != f'{year}-{month}-{day}':
+                continue
+            else:
+                event_start = event_end = over24Hdatetime(year, month, day, f'{hour_start}:{minute_start}')
+                return event_start, event_end
+    
+    return "", ""
 
 
 def over24Hdatetime(year, month, day, times):
     """
     24H以上の時刻をdatetimeに変換する
     """
-    hour, minute = times.split(":")[:-1]
+    hour, minute = times.split(":")
 
     # to minute
     minutes = int(hour) * 60 + int(minute)
@@ -128,23 +196,21 @@ def over24Hdatetime(year, month, day, times):
     dt = datetime.datetime(year=int(year), month=int(month), day=int(day))
     dt += datetime.timedelta(minutes=minutes)
 
-    return dt.strftime("%Y-%m-%dT%H:%M:%S")
+    return dt
 
 
 def prepare_info_for_calendar(
-    event_name_text, event_category_text, event_time_text
+    event_name, event_time, previous_add_event_lists, confirm
 ):
-    event_title = f"({event_category_text}){event_name_text}"
-    if event_time_text == "":
-        event_start = f"{year}-{month}-{event_date_text}"
-        event_end = f"{year}-{month}-{event_date_text}"
-        is_date = True
+    if (
+        f"{event_time}-{event_name}" in previous_add_event_lists
+    ):  
+        print("pass:" + event_time + " " + event_name)
+        return True
     else:
-        start, end = search_start_and_end_time(event_time_text)
-        event_start = over24Hdatetime(year, month, event_date_text, start)
-        event_end = over24Hdatetime(year, month, event_date_text, end)
-        is_date = False
-    return event_title, event_start, event_end, is_date
+        if confirm:
+            print("add:" + event_time + " " + event_name)
+        return False
 
 
 def change_event_starttime_to_jst(events):
@@ -157,24 +223,23 @@ def change_event_starttime_to_jst(events):
             event_jst_time = datetime.datetime.strptime(
                 str_event_uct_time, "%Y-%m-%dT%H:%M:%S+09:00"
             )
-            str_event_jst_time = event_jst_time.strftime("%Y-%m-%dT%H:%M:%S")
+            str_event_jst_time = event_jst_time.strftime("%Y-%m-%d")
             events_starttime.append(str_event_jst_time)
     return events_starttime
 
 
-def search_events(service, calendar_id, start):
+def search_events(service, calendar_id, start_datetime, end_datetime):
 
-    end_datetime = datetime.datetime.strptime(start, "%Y-%m-%d") + relativedelta(
-        months=1
-    )
-    end = end_datetime.strftime("%Y-%m-%d")
+    start_day = start_datetime.strftime("%Y-%m-%d")
+    end_day = end_datetime.strftime("%Y-%m-%d")
 
     events_result = (
         service.events()
         .list(
+            maxResults=2500,
             calendarId=calendar_id,
-            timeMin=start + "T00:00:00+09:00",  # NOTE:+09:00とするのが肝。（UTCをJSTへ変換）
-            timeMax=end + "T23:59:00+09:00",  # NOTE;来月までをサーチ期間に。
+            timeMin=start_day + "T00:00:00+09:00",  # NOTE:+09:00とするのが肝。（UTCをJSTへ変換）
+            timeMax=end_day + "T23:59:00+09:00",  # NOTE;来月までをサーチ期間に。
         )
         .execute()
     )
@@ -185,111 +250,108 @@ def search_events(service, calendar_id, start):
     else:
         events_starttime = change_event_starttime_to_jst(events)
         return [
-            event["summary"] + "-" + event_starttime
+            event_starttime + "-" +  event["summary"]
             for event, event_starttime in zip(events, events_starttime)
         ]
 
 
-def add_date_schedule(
-    event_name, event_category, event_time, event_link, previous_add_event_lists
-):
-    (
-        event_name_text,
-        event_category_text,
-        event_time_text,
-        event_link_text,
-    ) = search_detail_info(event_name, event_category, event_time, event_link)
+def add_info_to_calendar(calendarId, summary, event_day, event_start_time, event_end_time, event_link):
 
-    # カレンダーに反映させる情報の準備
-    (event_title, event_start, event_end, is_date,) = prepare_info_for_calendar(
-        event_name_text, event_category_text, event_time_text,
-    )
-
-    if (
-        f"{event_title}-{event_start}" in previous_add_event_lists
-    ):  # NOTE:同じ予定がすでに存在する場合はパス
-        print("pass:" + event_start + " " + event_title)
-        pass
-    else:
-        active_members = search_active_member(event_link_text)
-        print("add:" + event_start + " " + event_title)
-        add_info_to_calendar(
-            calendarId, event_title, event_start, event_end, active_members, event_link_text, is_date,
-        )
-
-
-def add_info_to_calendar(calendarId, summary, start, end, active_members, event_link_text, is_date):
-
-    if is_date:
+    if(event_start_time == ""):
         event = {
             "summary": summary,
-            "description": event_link_text + "\n" + active_members,
-            "start": {"date": start, "timeZone": "Japan",},
-            "end": {"date": end, "timeZone": "Japan",},
+            "description": f"{event_link}",
+            "start": {"date": event_day, "timeZone": "Japan",},
+            "end": {"date": event_day, "timeZone": "Japan",},
         }
     else:
         event = {
             "summary": summary,
-            "description": event_link_text + "\n" + active_members,
-            "start": {"dateTime": start, "timeZone": "Japan",},
-            "end": {"dateTime": end, "timeZone": "Japan",},
+            "description": f"{event_link}",
+            "start": {"dateTime": event_start_time.strftime("%Y-%m-%dT%H:%M:%S"), "timeZone": "Japan",},
+            "end": {"dateTime": event_end_time.strftime("%Y-%m-%dT%H:%M:%S"), "timeZone": "Japan",},
         }
 
     event = service.events().insert(calendarId=calendarId, body=event,).execute()
 
 
 if __name__ == "__main__":
-    #me = singleton.SingleInstance() 
+    me = singleton.SingleInstance() 
 
-    # -------------------------step1:各種設定-------------------------
     # API系
     calendarId = (
         os.environ['CALENDAR_ID']  # NOTE:自分のカレンダーID
     )
     service = build_calendar_api()
 
-    # サーチ範囲
-    num_search_month = 3  # NOTE;3ヶ月先の予定までカレンダーに反映
-    current_search_date = datetime.datetime.now()
-    year = current_search_date.year
-    month = current_search_date.month
+    schedule_list = get_schedule_list(1, 20)
 
-    # -------------------------step2.各日付ごとの情報を取得-------------------------
-    for _ in range(num_search_month):
-        month = "{:0=2}".format(int(month))
-        events_each_date = search_event_each_date(year, month)
-        if events_each_date == None:
+    if schedule_list == None:
+        sys.exit()
+
+    
+    for schedule in schedule_list:
+        start_day_buf = schedule.find("time", {"class": "time"})
+        if start_day_buf == None:
+            break
+        start_day_buf = start_day_buf['datetime']
+        if start_day_buf == None:
+            break
+        else:
+           start_day = start_day_buf
+
+    start_datetime = datetime.datetime.strptime(start_day, "%Y-%m-%d")
+    start_datetime = start_datetime + datetime.timedelta(days=-1)
+    end_day = schedule_list[0].find("time", {"class": "time"})['datetime']
+    end_datetime = datetime.datetime.strptime(end_day, "%Y-%m-%d")
+    end_datetime = end_datetime + datetime.timedelta(days=1)
+    previous_add_event_lists = search_events(service, calendarId, start_datetime, end_datetime)
+
+    for schedule in schedule_list:
+        (
+            event_day,
+            event_name,
+            event_link,
+        ) = get_schedule_info(schedule)
+
+        if (event_day == None or
+            event_name == None or
+            event_link == None):
             continue
-        for event_each_date in events_each_date:
 
-            # step3: 特定の日の予定を一括で取得
-            (
-                event_date_text,
-                events_time,
-                events_name,
-                events_category,
-                events_link,
-            ) = search_event_info(event_each_date)
+        if prepare_info_for_calendar(
+            event_name,
+            event_day,
+            previous_add_event_lists,
+            False,
+        ) == True:
+            continue
 
-            event_date_text = "{:0=2}".format(
-                int(event_date_text)
-            )  # NOTE;２桁になるように0埋め（ex.0-> 01）
-            start = f"{year}-{month}-{event_date_text}"
-            previous_add_event_lists = search_events(service, calendarId, start)
+        url = f'https://mihowatanabe.jp{event_link}'
+        (
+            event_start_time,
+            event_end_time,
+        )= get_schedule_time(event_day, url)
 
-            # step4: カレンダーへ情報を追加
-            for event_name, event_category, event_time, event_link in zip(
-                events_name, events_category, events_time, events_link
-            ):
-                add_date_schedule(
-                    event_name,
-                    event_category,
-                    event_time,
-                    event_link,
-                    previous_add_event_lists,
-                )
+        # 24時を跨ぐ時刻表記によって日付が変わっているためもう一度
+        if event_start_time != "":
+            if prepare_info_for_calendar(
+                event_name,
+                event_start_time.strftime("%Y-%m-%d"),
+                previous_add_event_lists,
+                True,
+            ) == True:
+                continue
+        else:
+            print("add:" + event_day + " " + event_name)
 
-        # step5:次の月へ
-        current_search_date = current_search_date + relativedelta(months=1)
-        year = current_search_date.year
-        month = current_search_date.month
+
+        # step4: カレンダーへ情報を追加
+        add_info_to_calendar(
+            calendarId,
+            event_name,
+            event_day,
+            event_start_time,
+            event_end_time,
+            url,
+        )
